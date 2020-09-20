ff19708199587f93122604583dc7350cf4456dc0
Austin Hodge <austindh1992@gmail.com>
2020-09-20 16:10:22 -0500

    Completed CountOfPositiveOddsBelowNumber

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -81,7 +81,15 @@ namespace ChallengeSets
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            int count = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (i % 2 == 1)
+                    count++;
+            }
+
+            return count;
         }
     }
 }
