c21bfea1c854203c5287a3db674205bd7509d697
ahodge92 <austindh1992@gmail.com>
2020-12-04 19:00:49 -0600

    Completed CountOfBusinessesWithNegativeNetProfit

--- ChallengeSets/ChallengeSet07.cs
+++ ChallengeSets/ChallengeSet07.cs
@@ -7,7 +7,24 @@ namespace ChallengeSets
     {
         public int CountOfBusinessesWithNegativeNetProfit(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            var count = 0;
+            var profit = 0.00;
+            if (businesses == null)
+            {
+                return  0;
+            }
+            
+
+            foreach (var business in businesses)
+            {
+                profit = business.TotalRevenue - business.TotalExpenses;
+                if (profit<0)
+                {
+                    count++;
+
+                }
+            }
+            return count;
         }
 
         public string GetCommaSeparatedListOfProfitableBusinesses(List<Business> businesses)
