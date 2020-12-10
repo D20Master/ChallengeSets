063cdeb77f13027fd992448b98d16933aff0ba7b
ahodge92 <austindh1992@gmail.com>
2020-12-09 18:23:49 -0600

    Completed GetTicTacToeWinner

--- ChallengeSets/ChallengeSet07.cs
+++ ChallengeSets/ChallengeSet07.cs
@@ -70,7 +70,39 @@ namespace ChallengeSets
         public enum TicTacToeResult { X, O, Draw }
         public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
         {
-            throw new NotImplementedException();
+            var backDiagonal = new List<char>();
+
+            backDiagonal.Add(finalBoard[0, 2]);
+            backDiagonal.Add(finalBoard[1, 1]);
+            backDiagonal.Add(finalBoard[2, 0]);
+
+            for (int y = 0; y < 3; y++)
+            {
+                var vertLine = new List<char>();
+                var horzLine = new List<char>();
+                var fordDiagonal = new List<char>();
+
+                for (int x = 0; x < 3; x++)
+                {
+                    vertLine.Add(finalBoard[x, y]);
+                    horzLine.Add(finalBoard[y, x]);
+                    fordDiagonal.Add(finalBoard[x, x]);
+                }
+
+                if (string.Join("", vertLine) == "XXX" || string.Join("", horzLine) == "XXX" || string.Join("", fordDiagonal) == "XXX" || string.Join("", backDiagonal) == "XXX")
+                {
+                    return TicTacToeResult.X;
+                }
+                var z = string.Join("", vertLine);
+
+                if (string.Join("", vertLine) == "OOO" || string.Join("", horzLine) == "OOO" || string.Join("", fordDiagonal) == "OOO" || string.Join("", backDiagonal) == "OOO")
+                {
+                    return TicTacToeResult.O;
+                }
+
+            }
+
+            return TicTacToeResult.Draw;
         }
 
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
